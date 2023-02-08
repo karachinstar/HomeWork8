fc632f6d1d2f380d54e6dd55b4b5eac81f4bf67f
Семён <karachinstar@yandex.ru>
2023-02-08 21:02:59 +0800

    Решение задачи 54

---
+++ Task54/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задача 54: 
+Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+Console.Write("Введите длину массива = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите ширину массива = ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение рандомного числа = ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение рандомного числа = ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[,] num = new int[m, n];
+int[] temp2 = new int[n];
+for (int i = 0; i < num.GetLength(0); i++)
+{
+    for (int j = 0; j < num.GetLength(1); j++)
+    {
+        num[i, j] = new Random().Next(min, max);
+        Console.Write($"{num[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        temp2[j] = num[i, j];
+    }
+    Array.Sort(temp2);
+    for (int k = 0; k < n; k++)
+    {
+        num[i, k] = temp2[k];
+        Console.Write($"{num[i, k]} ");
+    }
+    Console.WriteLine();
+}
